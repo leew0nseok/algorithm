5ca96d1091a6f168356388e5fe92050cbcac8866
leewonseok <101381258+leew0nseok@users.noreply.github.com>
2025-02-23 23:23:26 +0900

    [Silver IV] Title: 스택, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.py
+++ 백준/Silver/10828. 스택/스택.py
@@ -1,48 +1,20 @@
 import sys
 
-
-class Stack:
-    def __init__(self):
-        self.arr = []
-
-    def push(self, x):
-        self.arr.append(x)
-
-    def pop(self):
-        if len(self.arr) == 0:
-            print(-1)
-        else:
-            print(self.arr.pop())
-
-    def size(self):
-        print(len(self.arr))
-
-    def empty(self):
-        if len(self.arr) == 0:
-            print(1)
-        else:
-            print(0)
-
-    def top(self):
-      if len(self.arr) == 0:
-          print(-1)
-      else:
-          print(self.arr[-1])
-
-
 n = int(sys.stdin.readline())
 
-s = Stack()
+stack = []
 
-for _ in range(n):
-    cmd = sys.stdin.readline().split()
-    if cmd[0] == 'size':
-        s.size()
-    elif cmd[0] == 'top':
-        s.top()
-    elif cmd[0] == 'pop':
-        s.pop()
-    elif cmd[0] == 'empty':
-        s.empty()
-    elif cmd[0] == 'push':
-        s.push(cmd[1])
+for i in range(n):
+    x = sys.stdin.readline().split()
+    if x[0] == 'push':  # push operation
+        stack.append(int(x[1]))
+    elif x[0] == 'pop':
+        if len(stack) == 0: print(-1)
+        else: print(stack.pop())
+    elif x[0] == 'size': print(len(stack))
+    elif x[0] == 'empty': 
+        if len(stack) == 0: print(1)
+        else: print(0)
+    elif x[0] == 'top': 
+        if len(stack) == 0: print(-1)
+        else: print(stack[-1])
