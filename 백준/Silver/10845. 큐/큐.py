0cd56b2d196b9fb489fd2321bb9bf9f954518cc6
leewonseok <101381258+leew0nseok@users.noreply.github.com>
2025-02-26 00:35:11 +0900

    [Silver IV] Title: 큐, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.py
+++ 백준/Silver/10845. 큐/큐.py
@@ -4,29 +4,20 @@ n = int(sys.stdin.readline())
 
 queue = []
 
-for _ in range(n):
-    cmd = sys.stdin.readline().split()
-    if cmd[0] == 'size':
-        print(len(queue))
-    elif cmd[0] == 'front':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[0])
-    elif cmd[0] == 'back':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue[-1])
-    elif cmd[0] == 'empty':
-        if len(queue) == 0:
-            print(1)
-        else:
-            print(0)
-    elif cmd[0] == 'push':
-        queue.append(cmd[1])
-    elif cmd[0] == 'pop':
-        if len(queue) == 0:
-            print(-1)
-        else:
-            print(queue.pop(0))
+for i in range(n):
+    x = sys.stdin.readline().split()
+    if x[0] == 'push': 
+        queue.append(int(x[1]))
+    elif x[0] == 'pop':
+        if len(queue) == 0: print(-1)
+        else: print(queue.pop(0))
+    elif x[0] == 'size': print(len(queue))
+    elif x[0] == 'empty': 
+        if len(queue) == 0: print(1)
+        else: print(0)
+    elif x[0] == 'front': 
+        if len(queue) == 0: print(-1)
+        else: print(queue[0])
+    elif x[0] == 'back': 
+        if len(queue) == 0: print(-1)
+        else: print(queue[-1])
